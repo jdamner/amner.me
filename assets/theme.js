c108e068f0e0bce26c5b56a11b4cae2975c84a3b
jdamner <james@gluestudio.co.uk>
2020-05-02 11:40:33 +0100

    speed tweaks and ditched jQuery

--- assets/theme.js
+++ assets/theme.js
@@ -1,57 +1,45 @@
-jQuery(document).ready(function ($) {
-
-  var $grid = $('.grid').masonry({
-    // options
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true
-  });
-
-  $grid.imagesLoaded().progress(function () {
-    $grid.masonry('layout');
-  });
-
-  $(window).on('resize', function () {
-    if ($('.grid').length > 0) {
-      $grid.masonry('layout');
+// once documnet is ready (interactive), then load images as needed.
+document.addEventListener('readystatechange', event => {
+  if (event.target.readyState === "interactive") {
+    // lazy load images
+    var lazy = document.getElementsByClassName('lazy');
+    for (var i = 0; i < lazy.length; i++) {
+      lazy[i].src = lazy[i].getAttribute('data-src');
+      lazy[i].removeAttribute('data-src');
     }
-  })
 
-  window.setInterval(function () {
-    if ($('.grid').length > 0) {
-      $grid.masonry('layout');
-    }
-  }, 1000);
 
-  $(".nav-link,.dropdown-item").click(function () {
-    var t = $(this).attr("href");
-    $('.active').removeClass('active');
-    $("html, body").animate({
-      scrollTop: $(t).offset().top - 50
-    }, {
-      duration: 300,
-    });
+    var elem = document.querySelector('.grid')
+    if (elem.length > 0) {
+      console.log(elem)
+      var msnry = new Masonry(elem, {
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+      });
+      msnry.imagesLoaded().progress(function () {
+        msnry.masonry('layout');
+      })
+      window.setInterval(function () {
+        msnry.masonry('layout');
+      }, 1000);
 
-    $('body').scrollspy({
-      target: '#header-navbar',
-      offset: $(t).offset().top
-    });
-
-    animateHeader();
-    $(this).parent().addClass('active');
-    return false;
-  });
-
-  function animateHeader() {
-    var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
-    if (scroll < 10) {
-      $('#masthead').addClass('top')
-    } else {
-      $('#masthead').removeClass('top')
+      window.onresize = function () {
+        msnry.masonry('layout');
+      }
     }
   }
 
-  $(document).on('scroll', function (e) {
-    animateHeader();
-  });
+
+
+
+
+  window.onscroll = function () {
+    var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
+    if (scroll < 30) {
+      document.getElementById('masthead').classList.add('top')
+    } else {
+      document.getElementById('masthead').classList.remove('top')
+    }
+  }
 });
