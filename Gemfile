dd90a155ac4b8a89c130f143d8d6521044a80d91
jdamner <james@gluestudio.co.uk>
2020-05-02 13:50:20 +0100

    slimmed down everything to make it a bit more tidier

--- Gemfile
+++ Gemfile
@@ -8,9 +8,6 @@ source "https://rubygems.org"
 # This will help ensure the proper Jekyll version is running.
 # Happy Jekylling!
 gem "jekyll", "~> 4.0.0"
-# This is the default theme for new Jekyll sites. You may change this to anything you like.
-gem "minima", "~> 2.5"
-gem "amner-me", git: "https://d3ba859c85b35b7cd478164fbeeff25cca6c793b:x-oauth-basic@github.com/jdamner/amner-me-theme"
 # If you want to use GitHub Pages, remove the "gem "jekyll"" above and
 # uncomment the line below. To upgrade, run `bundle update github-pages`.
 # gem "github-pages", group: :jekyll_plugins
--- assets/theme.js
+++ assets/theme.js
@@ -1,4 +1,5 @@
 // once documnet is ready (interactive), then load images as needed.
+
 document.addEventListener('readystatechange', event => {
   if (event.target.readyState === "interactive") {
     // lazy load images
@@ -8,38 +9,13 @@ document.addEventListener('readystatechange', event => {
       lazy[i].removeAttribute('data-src');
     }
 
-
-    var elem = document.querySelector('.grid')
-    if (elem.length > 0) {
-      console.log(elem)
-      var msnry = new Masonry(elem, {
-        itemSelector: '.grid-item',
-        columnWidth: '.grid-sizer',
-        percentPosition: true
-      });
-      msnry.imagesLoaded().progress(function () {
-        msnry.masonry('layout');
-      })
-      window.setInterval(function () {
-        msnry.masonry('layout');
-      }, 1000);
-
-      window.onresize = function () {
-        msnry.masonry('layout');
+    window.onscroll = function () {
+      var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
+      if (scroll < 30) {
+        document.getElementById('masthead').classList.add('top')
+      } else {
+        document.getElementById('masthead').classList.remove('top')
       }
-    }
-  }
-
-
-
-
-
-  window.onscroll = function () {
-    var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
-    if (scroll < 30) {
-      document.getElementById('masthead').classList.add('top')
-    } else {
-      document.getElementById('masthead').classList.remove('top')
     }
   }
 });
