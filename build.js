77c949b7d29703a685a4d261e57195b483541e96
James Amner <jamner@spindogs.com>
2020-12-31 14:13:16 +0000

    Removed JS from the site, made everything a liot simpler!

--- build.js
+++ build.js
@@ -1,8 +1,5 @@
 const uncss = require('uncss');
 const fs = require('fs');
-const sha256 = require('js-sha256').sha256;
-const firebase_config = require('./.firebase/firebase.json');
-const base64 = require('base-64');
 
 
 var files   = ["_site/**/*.html"],
@@ -16,15 +13,4 @@ var files   = ["_site/**/*.html"],
 
 uncss(files, options, function (error, output) {
     fs.writeFileSync("_site/assets/style.css", output);
-});
-
-
-fs.readFile("_includes/javascript.html", function (error, data) {
-    data = String(data);
-    data = data.replace("<script>\n", "");
-    data = data.replace("\n</script>", "");
-    hash = sha256(data);
-    hash = base64.encode(hash);
-    new_firebase = JSON.stringify(firebase_config).replace("{{SHA_HASH}}", hash);
-    fs.writeFileSync('./firebase.json', new_firebase);
 });
