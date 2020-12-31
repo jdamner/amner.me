7be6299bd134793fec7abbc9fafb3218983c657a
James Amner <jamner@spindogs.com>
2020-12-31 14:00:46 +0000

    Base64 Encode the hash

--- build.js
+++ build.js
@@ -1,5 +1,8 @@
 const uncss = require('uncss');
 const fs = require('fs');
+const sha256 = require('js-sha256').sha256;
+const firebase_config = require('./.firebase/firebase.json');
+const base64 = require('base-64');
 
 
 var files   = ["_site/**/*.html"],
@@ -15,15 +18,13 @@ uncss(files, options, function (error, output) {
     fs.writeFileSync("_site/assets/style.css", output);
 });
 
-const sha256 = require('js-sha256').sha256;
-const firebase = require('./.firebase/firebase.json');
-var hash = "";
 
 fs.readFile("_includes/javascript.html", function (error, data) {
     data = String(data);
     data = data.replace("<script>\n", "");
     data = data.replace("\n</script>", "");
     hash = sha256(data);
-    new_firebase = JSON.stringify(firebase).replace("{{SHA_HASH}}", hash);
+    hash = base64.encode(hash);
+    new_firebase = JSON.stringify(firebase_config).replace("{{SHA_HASH}}", hash);
     fs.writeFileSync('./firebase.json', new_firebase);
 });
