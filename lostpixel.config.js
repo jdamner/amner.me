f9530b5c9ffec283091a1d30d1724a171d87d2ca
James Amner <james.amner@boxuk.com>
2022-12-07 15:45:05 +0000

    config

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -1,20 +1,25 @@
+let viewport = {
+  width: 1920,
+  height: 2480,
+}
+
 module.exports = {
   pageShots: {
     pages: [
-      { path: "/", name: "Home" },
-      { path: "/404", name: "404" },
-      { path: "/chasing-100", name: "Chasing 100" },
-      { path: "/full-site-editing-in-wordpress", name: "Full Site Editing in WordPress" },
-      { path: "/how-i-publish-this-site", name: "How I Publish This Site" },
-      { path: "/ive-lauched-colorbar", name: "I've Launched Colorbar" },
-      { path: "/migration-to-firebase", name: "Migration to Firebase" },
-      { path: "/new-wp-plugin", name: "New WP Plugin" },
-      { path: "/whats-lurking-inside-your-project", name: "What's Lurking Inside Your Project?" },
-      { path: "/why-not-wordpress", name: "Why Not WordPress?" },
+      { path: "/", name: "Home", viewport },
+      { path: "/404", name: "404", viewport },
+      { path: "/chasing-100", name: "Chasing 100", viewport },
+      { path: "/full-site-editing-in-wordpress", name: "Full Site Editing in WordPress", viewport },
+      { path: "/how-i-publish-this-site", name: "How I Publish This Site", viewport },
+      { path: "/ive-lauched-colorbar", name: "I've Launched Colorbar", viewport },
+      { path: "/migration-to-firebase", name: "Migration to Firebase", viewport },
+      { path: "/new-wp-plugin", name: "New WP Plugin", viewport },
+      { path: "/whats-lurking-inside-your-project", name: "What's Lurking Inside Your Project?", viewport },
+      { path: "/why-not-wordpress", name: "Why Not WordPress?", viewport },
     ],
     baseUrl: "http://172.17.0.1:3000",
   },
   generateOnly: true,
   failOnDifference: true,
-  waitBeforeScreenshot: 30000,
+  waitBeforeScreenshot: 5000,
 };
