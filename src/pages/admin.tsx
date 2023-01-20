2df6446b0c621c40a272f26ee5d496a1162bb663
James Amner <james.amner@boxuk.com>
2023-01-20 12:41:35 +0000

    Admin Loading

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -4,20 +4,22 @@ import dynamic from 'next/dynamic';
 /* Config */
 import config from '../config/cms';
 
+/* Components */
+const loading = () => <></>;
+
 /**
  * Load the CMS Dynamically
  * 
  * It won't work for server-side rendering becaue it relies on `window`. 
  */
-export default function CMS() {
+export default function CMS(): JSX.Element {
 	const CMS = dynamic(
 		async () => {
 			const netlifly = await import('netlify-cms-app');
-			const cms = netlifly.default;
-			cms.init({ config });
-			return () => <></>
+			netlifly.default.init({ config });
+			return loading;
 		},
-		{ ssr: false, loading: () => <></> },
+		{ ssr: false, loading },
 	);
 	return <CMS />;
 }
