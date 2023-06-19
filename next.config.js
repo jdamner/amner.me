19985075652c1802de3f3f7f546638ea7ed8c62f
James Amner <james.amner@boxuk.com>
2023-06-19 17:18:25 +0100

    WIP

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
  * @type {import('next').NextConfig}
  **/
 module.exports = withBundleAnalyzer({
-	output: 'export',
 	reactStrictMode: true,
 	poweredByHeader: false,
 	trailingSlash: false,
